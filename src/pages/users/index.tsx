5c13f5e871fa44289bc40a243895ceb227549c03
Caio Oliveira <caiolv@me.com>
2021-12-01 09:48:03 -0300

    add: pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -18,6 +18,7 @@ import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
 import Header from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
+import Pagination from '../../components/Pagination';
 
 export default function UserList() {
     return (
@@ -79,6 +80,8 @@ export default function UserList() {
                             </Tr>
                         </Tbody>
                     </Table>
+
+                    <Pagination />
                 </Box>
             </Flex>
         </Box>
