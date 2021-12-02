61658f51c8c6551a75a9fb876674701fb6986b99
Caio Oliveira <caiolv@me.com>
2021-12-02 10:32:08 -0300

    add: forward ref

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,3 +1,4 @@
+import { forwardRef, ForwardRefRenderFunction } from 'react';
 import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps } from '@chakra-ui/react';
 
 interface InputProps extends ChakraInputProps {
@@ -5,7 +6,8 @@ interface InputProps extends ChakraInputProps {
     label?: string;
 }
 
-export function Input({ name, label, ...rest } : InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps>
+  = ({ name, label, ...rest }, ref) => {
     return (
         <FormControl>
             { !!label && <FormLabel forHtml="email">{label}</FormLabel>}
@@ -19,8 +21,11 @@ export function Input({ name, label, ...rest } : InputProps) {
                 bgColor: 'gray.900'
               }}
               size="lg"
+              ref={ref}
               {...rest}
             />
           </FormControl>
     );
 }
+
+export const Input = forwardRef(InputBase);
