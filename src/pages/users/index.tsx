9f0a9e8006e83513ea87b54d96c7e1be7e7ffbee
Caio Oliveira <caiolv@me.com>
2021-12-06 11:01:15 -0300

    add: axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -25,11 +25,11 @@ import { useQuery } from 'react-query'
 import Header from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import Pagination from '../../components/Pagination';
+import { api } from '../../services/api';
 
 export default function UserList() {
     const { data, isLoading, isFetching, error, refetch } = useQuery('users', async () => {
-        const response = await fetch('http://localhost:3000/api/users');
-        const data = await response.json();
+        const { data } = await api.get('users');
 
         const users = data.users.map(user => {
             return {
