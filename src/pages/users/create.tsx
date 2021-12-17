8d568542d91adad1a4c1d9a7d2894d0a24416bd2
Caio Oliveira <caiolv@me.com>
2021-12-17 16:56:31 -0300

    add: mutations

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -10,13 +10,17 @@ import {
     Button,
 } from '@chakra-ui/react';
 import Link from 'next/link';
+import * as yup from 'yup';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useMutation } from 'react-query'
 
 import Header from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { Input } from '../../components/Form/Input';
-import * as yup from 'yup';
 import {yupResolver} from '@hookform/resolvers/yup/dist/yup';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { api } from '../../services/api';
+import { queryClient } from '../../services/queryClient';
+import { useRouter } from 'next/router';
 
 type CreateUserFormData = {
   name: string;
@@ -36,14 +40,33 @@ const createUserFormSchema = yup.object().shape({
 });
 
 export default function CreateUser() {
+    const router = useRouter();
+
+    const createUser = useMutation(async (user: CreateUserFormData) => {
+        const response = await api.post('users', {
+            user: {
+                ...user,
+                created_at: new Date(),
+            }
+        });
+
+
+        return response.data.user;
+    }, {
+        onSuccess: () => {
+            queryClient.invalidateQueries(['users']);
+        },
+    });
+
     const { register, handleSubmit, formState } = useForm({
         resolver: yupResolver(createUserFormSchema)
     });
     const { errors } = formState;
 
     const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-        await new Promise(resolve => setTimeout(resolve, 2000));
-        console.log(values)
+        await createUser.mutateAsync(values);
+
+        router.push('/users');
     }
 
     return (
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model, Response } from "miragejs";
+import { createServer, Factory, Model, Response, ActiveModelSerializer } from "miragejs";
 import faker from 'faker';
 
 type User = {
@@ -9,6 +9,11 @@ type User = {
 
 export function makeServer() {
   const server = createServer({
+
+    serializers: {
+      application: ActiveModelSerializer,
+    },
+
     models: {
       user: Model.extend<Partial<User>>({}),
     },
@@ -44,7 +49,9 @@ export function makeServer() {
         const pageEnd = pageStart + Number(per_page);
 
         const users = this.serialize(schema.all('user'))
-        .users.slice(pageStart, pageEnd);
+          .users
+          .sort((a, b) => a.createdAt - b.createAt)
+          .slice(pageStart, pageEnd);
 
         return new Response(
           200,
